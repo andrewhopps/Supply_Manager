fee5ecb7fd4b314bc768ecb29550347ba0e04106
Andrew Hopps <andrewhopps@gmail.com>
2017-07-09 03:08:46 +0000

    Input?

--- index.php
+++ index.php
@@ -138,7 +138,7 @@ $result = $mysqli->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "SKU: " . $row["SKU"] . " - Description: " . $row["Description"] . "<input type="text"><br>";
+        echo "SKU: " . $row["SKU"] . " - Description: " . $row["Description"] . "<br>";
         
     }
 }
